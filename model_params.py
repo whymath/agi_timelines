53a7198a5d80850fc027e89efe24bcd62f7d6e4b
whymath <20425672+whymath@users.noreply.github.com>
2026-01-15 17:18:16 -0500

    Minor changes to invert reliability penalty calc array

--- model_params.py
+++ model_params.py
@@ -12,6 +12,7 @@ from model_data import model_data
 
 # HACCA flags to set
 hacca_mode = True
+invert_reliability_penalty = False
 custom_doubling_time_mode = False
 
 # Original defaults
@@ -92,6 +93,8 @@ def reliability_count_to_penalty(reliability):
     r = np.asarray(reliability, dtype=float)
     reliability = np.array([0.50, 0.80, 0.90, 0.95, 0.99])
     penalty = np.array([1.0, 0.25, 0.25**2, 0.25**3, 0.25**4])
+    if invert_reliability_penalty:
+        penalty = np.array([1.0, 4, 4**2, 4**3, 4**4])
     matches = r[..., None] == reliability
     hit_any = matches.any(axis=-1)
     idx = matches.argmax(axis=-1)
